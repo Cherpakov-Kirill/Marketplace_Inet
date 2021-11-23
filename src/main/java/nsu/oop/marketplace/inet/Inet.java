f5ee986a752cf72452282278680ab504fc5484bc
Kirill <cherpakov-kirill-42@mail.ru>
2021-11-23 13:41:58 +0700

    Implementation settings

---
+++ src/main/java/nsu/oop/marketplace/inet/Inet.java
@@ -0,0 +1,11 @@
+package nsu.oop.marketplace.inet;
+
+public class Inet {
+    private final int a;
+    public Inet(int a){
+        this.a = a;
+    }
+    public int getA(){
+        return a;
+    }
+}
